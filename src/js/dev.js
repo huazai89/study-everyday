cac5fdd6cf3ee5471e46591c93261a05791a38c1
付兴华 <fuxinghua@xiaomi.com>
2019-07-17 09:49:03 +0800

    [update] dev

--- src/js/dev.js
+++ src/js/dev.js
@@ -2,6 +2,8 @@ console.info('dev branch')
 console.info('test diff');
 console.info(23);
 console.info(456);
-function f () {
-    return 'test';
+function f() {
+    let a = 2,
+        b = 3;
+    return a + b;
 }
