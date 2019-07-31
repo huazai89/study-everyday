c0287ff7f08456bfe0ae5ba5ff5ece129a5fbb86
付兴华 <fuxinghua@xiaomi.com>
2019-07-31 15:37:09 +0800

    [ADD]

--- src/js/getUsername.js
+++ src/js/getUsername.js
@@ -1,2 +1,12 @@
 console.info('my name is test')
 console.info('add')
+
+let userInfo = {
+	name: 'zhangsan',
+	age: 25,
+	dept: 'XM'
+}
+console.info(Object.keys(userInfo))
+Object.keys(userInfo).map((item, index) => {
+	console.info(item, index)
+})
