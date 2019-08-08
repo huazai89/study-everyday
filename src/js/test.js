aa7b7c21ac8ef8ebe1c72c8f1fa1710e3f2c21db
付兴华 <fuxinghua@xiaomi.com>
2019-08-08 18:08:57 +0800

    [Add]

--- src/js/test.js
+++ src/js/test.js
@@ -1,1 +1,20 @@
+/*
+ * @Description: 
+ * @Version: 1.0
+ * @Autor: fuxinghua@xiaomi.com
+ * @Date: 2019-07-17 09:11:24
+ * @LastEditors: 
+ * @LastEditTime: 2019-07-17 09:11:24
+ */
+
+
 console.info('test')
+
+decodeURIComponent(null)
+/**
+ * @description: 
+ * @param {type} 
+ * @return: 
+ * @author: fuxinghua@xiaomi.com
+ */
+function test(a, b) { }
